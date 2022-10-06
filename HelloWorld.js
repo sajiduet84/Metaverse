fdd443ae7ba9f6f1c1680f17bf4c45b8267ae2a7
Sajid Mahmood <sajid.mahmood.bth@gmail.com>
2022-10-06 11:34:50 +0500

    adding file HelloWorld.js

---
+++ HelloWorld.js
@@ -0,0 +1,14 @@
+/*
+This is my first program in JavaScript.
+HelloWorld.JS
+
+Author: Sajid Mahmood
+PIAIC
+*/
+
+
+/*
+Pring Hello World on Web browser
+*/
+
+console.log("Hello World, JavaScript");
